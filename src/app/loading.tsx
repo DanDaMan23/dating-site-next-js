34dee80fcc7c385d8f9749e3906cf3358b26ffcc
DanDaMan23 <danlawrcons.gmail.com>
2024-01-04 18:37:48 -0600

    Added loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <main className='flex justify-center items-center h-screen bg-gray-300 bg-opacity-40'>
+      <div className='animate-spin rounded-full h-20 w-20 border-t-2 border-white' />
+    </main>
+  )
+}
